28730a57fd11a4272f93f7e518bb6827c27378f9
do-haru <doharu05@naver.com>
2026-02-18 15:08:20 +0900

    feat: 레이아웃 컴포넌트(Main, Footer) 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
 import "./App.css";
+import Main from "./components/Main";
+import Footer from "./components/Footer";
 
 function App() {
-  return <>Random-eats</>;
+  return (
+    <div className="App">
+      <main className="AppMain">
+        <Main />
+      </main>
+      <footer className="AppFooter">
+        <Footer />
+      </footer>
+    </div>
+  );
 }
 
 export default App;
